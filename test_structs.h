e862cf04b053451fcfca4c05f22456f2e6179b4a
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2015-06-25 14:00:51 +0800

    Added struct converter and example file

---
+++ test_structs.h
@@ -0,0 +1,17 @@
+typedef struct
+{
+ 	bool boolean_value;
+  	char char_value;
+  	char* string_value;
+  	int int_value;
+  	float float_value;
+  	double double_value;
+  	uint8_t uint8_value;
+  	uint16_t uint16_value;
+  	uint32_t uint32_value;
+  	uint64_t uint64_value;
+  	int8_t int8_value;
+  	int16_t int16_value;
+  	int32_t int32_value;
+  	int64_t int64_value;
+} test_struct;
